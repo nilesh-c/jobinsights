aa049b5aeda85cabd76d4a5ebb0b6cfbe0689461
Nilesh Chakraborty <nilesh@nileshc.com>
2014-02-19 22:59:35 +0530

    Support OPENSHIFT python ip

--- lcrawler/src/changeip.py
+++ lcrawler/src/changeip.py
@@ -3,7 +3,8 @@ import time, sys
 
 # for i in range(9060, 9066):
 i = sys.argv[1]
-tn = Telnet('127.0.0.1', i)
+serverIP = os.environ['OPENSHIFT_PYTHON_IP'] if ('OPENSHIFT_PYTHON_IP' in os.environ) else '127.0.0.1'
+tn = Telnet(serverIP, i)
 tn.write("AUTHENTICATE\r\n")
 tn.read_until("250 OK", 2)
 tn.write("signal NEWNYM\r\n")
