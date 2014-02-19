eaa3a78e4283d28ce4da2c861d843786211f6a3b
Nilesh Chakraborty <nilesh@nileshc.com>
2014-02-19 13:07:55 +0530

    Specify Tor control port through CLI arg

--- lcrawler/src/changeip.py
+++ lcrawler/src/changeip.py
@@ -1,11 +1,13 @@
 from telnetlib import Telnet
-import time
+import time, sys
 
-tn = Telnet('127.0.0.1', 9051)
+# for i in range(9060, 9066):
+i = sys.argv[1]
+tn = Telnet('127.0.0.1', i)
 tn.write("AUTHENTICATE\r\n")
 tn.read_until("250 OK", 2)
 tn.write("signal NEWNYM\r\n")
 tn.read_until("250 OK", 2)
 tn.write("QUIT\r\n")
 tn.close()
-print "Tor IP changed!"
+print "Tor IP for port %d changed!" % i
